4b34851eb599229ba4a589c6e2d47043c3f6811c
Hadeel <hadeel.has97@gmail>
2020-08-29 05:30:27 +0300

    updated

--- main.py
+++ main.py
@@ -93,7 +93,8 @@ def decode_jwt():
     token = str.replace(str(data), 'Bearer ', '')
     try:
         data = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-    except: # pylint: disable=bare-except
+    except: 
+        # pylint: disable=bare-except
         abort(401)
 
 
